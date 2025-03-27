2cf18a62d8a6e19b153d080d2d8df0223199756b
Piotr Kosmowski <pkosmowski553@gmail.com>
2025-03-27 20:38:44 +0100

    api endpoint protected

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,37 @@ const app = express();
 
 
 var {NodeAdapter} = require("ef-keycloak-connect");
-const config = require(`${Path_To_Config_File}`);
+const config = {
+        "realm": "projekt-realm",
+        "auth-server-url": "http://localhost:8180:port/auth/",
+        "ssl-required": "external",
+        "resource": "keycloak_resource_name",
+        "verify-token-audience": false,
+        "credentials": {
+          "secret": "dRoI794oild4nYxUKMjv1aQL1aqmwSGj"
+        },
+        "use-resource-role-mappings": true,
+        "confidential-port": 0,
+        "policy-enforcer": {},
+        "CLIENT_ID": "angular-app",
+        "CLIENT_DB_ID": "461d914e-4b22-4978-8c67-7fe0dfe45d86",
+        "GRANT_TYPE": "password",
+        "GRANT_TYPE_PAT": "client_credentials",
+        "USERNAME_ADMIN": "admin_name",
+        "PASSWORD_ADMIN": "admin_password",
+        "SCOPE_NAME": "Any default scope",
+        "bearer-only": true
+      }
 const keycloak = new NodeAdapter(config)
-
-app.use(keycloak.middleware());
 
 app.get('/api', keycloak.protect(), (req, res) => {
   res.json({message: 'Hello from API'});
 });
 
-app.listen(3000, () => {
-  console.log('Server started on http://localhost:3000');
+app.get('/hello', (req, res) => {
+    res.json({message: 'Hello!'});
+});
+
+app.listen(5000, () => {
+  console.log('Backend: http://localhost:5000');
 });
